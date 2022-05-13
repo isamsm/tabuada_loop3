7503d8b6bc5c213a7accc8e825a9a013d9d262b8
Isa <isamarsques@gmail.com>
2022-05-12 22:44:22 -0300

    tabuada loopV3

---
+++ java.js
@@ -0,0 +1,15 @@
+document.write("<table border='1' align='center'>")
+
+for (let x = 2; x <= 20; x++) {
+    document.write("<tr>")
+    if (x % 2 == 0) {
+        for (let y = 1; y <= 10; y++) {
+        document.write("<td class='coluna"+y/2+"'>")
+        document.write(y + ' * ' + x + ' = ' + (x * y))
+        document.write("</td>")
+        }
+    }
+}
+
+document.write("</tr>")
+document.write("</table>")
