39843748c50371a72b6c44ae0eeb0dc6794c7173
Isa Marques <80493617+isamsm@users.noreply.github.com>
2022-05-15 22:55:55 -0300

    Update java.js

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ for (let x = 2; x <= 20; x++) {
     document.write("<tr>")
     if (x % 2 == 0) {
         for (let y = 1; y <= 10; y++) {
-        document.write("<td class='coluna"+y/2+"'>")
+        document.write("<td>")
         document.write(y + ' * ' + x + ' = ' + (x * y))
         document.write("</td>")
         }
